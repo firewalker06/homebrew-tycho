b07b289da7d60909a6d727c6dfcb05cedd40130c
firewalker06 <didik.wicaksono@gmail.com>
2026-07-05 10:33:05 +0700

    tycho 0.7.1
    Closes #13.
    
    Signed-off-by: Didik Wicaksono <didik.wicaksono@gmail.com>

--- Formula/tycho.rb
+++ Formula/tycho.rb
@@ -1,17 +1,10 @@
 class Tycho < Formula
   desc "Local-first coding agent supervisor and scheduler"
   homepage "https://github.com/firewalker06/tycho"
-  url "https://github.com/firewalker06/tycho/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "1fcc10acdf958fe5f255c13b86b0f5375e25452573789ab196c575a8c2f5fcea"
+  url "https://github.com/firewalker06/tycho/archive/refs/tags/v0.7.1.tar.gz"
+  sha256 "18406c70f52bd3b04a69ac9640fa333841e4693491c6cb3a807fe00e12dd7852"
   license "MIT"
   head "https://github.com/firewalker06/tycho.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/firewalker06/homebrew-tycho/releases/download/tycho-0.7.0"
-    sha256 cellar: :any, arm64_tahoe:  "8c3984f30fcc0afb7a6d0d2adaa001ce79a02c6e2ae9e4e344e5f5db29ce933d"
-    sha256 cellar: :any, sequoia:      "f5eded85ab96521cbc8fef19aa670f1186c5f1eb57ca5ab9ad552be3117a1b9a"
-    sha256 cellar: :any, x86_64_linux: "a8aaa96ee7f8dea2dba931c3b91500755d0c08d064af82d3478cc556014d1b20"
-  end
 
   depends_on "go" => :build
   depends_on "openssl@3"
