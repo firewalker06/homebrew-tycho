f6961a855668a44db098b704a1d548fd43811360
Didik Wicaksono <didik.wicaksono@gmail.com>
2026-07-06 00:35:33 +0000

    tycho: add 0.7.3 bottle.

--- Formula/tycho.rb
+++ Formula/tycho.rb
@@ -6,6 +6,13 @@ class Tycho < Formula
   license "MIT"
   head "https://github.com/firewalker06/tycho.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/firewalker06/homebrew-tycho/releases/download/tycho-0.7.3"
+    sha256 cellar: :any, arm64_tahoe:  "79af3a3df617086e15def58bf9436d2c7158c314af75296bfd6e6d7ef891388a"
+    sha256 cellar: :any, sequoia:      "2c8a4e9a222ec468881f230678e5b47bac3905c235fdf24007f83ed885997035"
+    sha256 cellar: :any, x86_64_linux: "18c43c21dcb8a5fd204a784d4a4576a5ea2b47f7423ba5d48f2198ee528f2251"
+  end
+
   depends_on "go" => :build
   depends_on "openssl@3"
   depends_on "ruby"
